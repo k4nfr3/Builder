2c0127d5c95ff0e1ff36abb69ad7862520d03c2b
k4nfr3 <45294916+k4nfr3@users.noreply.github.com>
2021-12-02 16:01:52 +0100

    Update main.bat

--- main.bat
+++ main.bat
@@ -3,40 +3,38 @@ CALL pre-install.bat
 
 
 :: Build impacket
-REM CALL clone.bat SecureAuthCorp/impacket
+CALL clone.bat SecureAuthCorp/impacket
 cd examples
-REM CALL build-py.bat wmiexec , wmiexec , 0
-REM CALL build-py.bat secretsdump , secretsdump , 0
-REM CALL build-py.bat smbserver , smbserver , 0
-REM CALL build-py.bat smbclient , smbclient , 0
-REM CALL build-py.bat smbexec , smbexec , 0
-REM CALL build-py.bat psexec , psexec , 0
-REM CALL build-py.bat dcomexec , dcomexec , 0
-REM CALL build-py.bat GetUserSPNs , GetUserSPNs , 0
-REM CALL build-py.bat GetNPUsers , GetNPUsers , 0
-REM CALL build-py.bat getST , getST , 0
-REM CALL build-py.bat getTGT , getTGT , 0
-REM CALL build-py.bat ticketer , ticketer , 0
+CALL build-py.bat wmiexec , wmiexec , 0
+CALL build-py.bat secretsdump , secretsdump , 0
+CALL build-py.bat smbserver , smbserver , 0
+CALL build-py.bat smbclient , smbclient , 0
+CALL build-py.bat smbexec , smbexec , 0
+CALL build-py.bat psexec , psexec , 0
+CALL build-py.bat dcomexec , dcomexec , 0
+CALL build-py.bat GetUserSPNs , GetUserSPNs , 0
+CALL build-py.bat GetNPUsers , GetNPUsers , 0
+CALL build-py.bat getST , getST , 0
+CALL build-py.bat getTGT , getTGT , 0
+CALL build-py.bat ticketer , ticketer , 0
 
 
-REM CALL clone.bat NinjaStyle82/rbcd_permissions
-REM CALL build-py.bat rbcd_permissions , rbcd_permissions , 0
+CALL clone.bat NinjaStyle82/rbcd_permissions
+CALL build-py.bat rbcd , rbcd , 0
 
 
-REM CALL clone.bat k4nfr3/WebclientServiceScanner
-REM cd webclientservicescanner
-REM CALL build-py.bat console , WebclientServiceScanner , 0
+CALL clone.bat Hackndo/WebclientServiceScanner
+cd webclientservicescanner
+git am %scriptpath%\WebclientServiceScanner\0001-Add-color-by-k4nfr3-WebclientServiceScanner.patch
+CALL build-py.bat console , WebclientServiceScanner , 0
 
-REM CALL clone.bat login-securite/DonPAPI
-REM cd DonPAPI
-REM CALL build-py.bat DonPAPI , DonPAPI , 0
 
 :: See https://dirkjanm.io/krbrelayx-unconstrained-delegation-abuse-toolkit/
-REM CALL clone.bat dirkjanm/krbrelayx
-REM CALL build-py.bat krbrelayx , krbrelayx , 0
-REM CALL build-py.bat printerbug , printerbug , 0
-REM CALL build-py.bat dnstool , dnstool , 0
-REM CALL build-py.bat addspn , addspn , 0
+CALL clone.bat dirkjanm/krbrelayx
+CALL build-py.bat krbrelayx , krbrelayx , 0
+CALL build-py.bat printerbug , printerbug , 0
+CALL build-py.bat dnstool , dnstool , 0
+CALL build-py.bat addspn , addspn , 0
 
 
 :: Build pypykatz
@@ -46,26 +44,24 @@ CALL clone.bat skelsec/pypykatz
 :: https://skelsec.medium.com/play-with-katz-get-scratched-6c2c350fadf2
 :: https://drive.google.com/drive/folders/1KT2yWziJHvaH41jtZMsatey2KycWF824?usp=sharing
 :: From https://github.com/skelsec/pypykatz/commit/f53ed8c691b32c2a5a0189604d56afe4732fb639
-git config user.email "appveyor@appveyor-vm.com"
-git config user.name "k4nfr3"
 cd pypykatz
 git am %scriptpath%\pypykatz\0001-xdrprotection.patch
 git am %scriptpath%\pypykatz\0001-Add-debug-message-for-method-handledup.patch
 CALL build-py.bat __main__ , pypykatz , 0
 
 
-REM CALL clone.bat skelsec/kerberoast
-REM cd kerberoast
-REM CALL build-py.bat kerberoast , kerberoast , 0
+CALL clone.bat skelsec/kerberoast
+cd kerberoast
+CALL build-py.bat kerberoast , kerberoast , 0
 
 
 :: Build BloodHound
-REM CALL clone.bat fox-it/BloodHound.py
+CALL clone.bat fox-it/BloodHound.py
 :: Patch bloodhound to avoid "unrecognized arguments: --multiprocessing-fork"
 :: In case where the patch doesn't work DO NOT USE "-c ALL" and avoid DCOnly and ACL. Use -c "Group,LocalAdmin,Session,Trusts,DCOM,RDP,PSRemote,LoggedOn,ObjectProps"
 :: Maybe the argument "--disable-pooling" can do the tricks
-REM %py64% -c "f=open('bloodhound/__init__.py','r');d=f.read().replace('    main()','    import multiprocessing;multiprocessing.freeze_support();main()');f.close();f=open('bloodhound/__init__.py','w').write(d);f.close();"
-REM CALL build-py.bat bloodhound, bloodhound , 0
+%py64% -c "f=open('bloodhound/__init__.py','r');d=f.read().replace('    main()','    import multiprocessing;multiprocessing.freeze_support();main()');f.close();f=open('bloodhound/__init__.py','w').write(d);f.close();"
+CALL build-py.bat bloodhound, bloodhound , 0
 
 
 :: DISABLED => See https://github.com/fox-it/mitm6/issues/3
@@ -103,12 +99,12 @@ REM CALL build-py.bat bloodhound, bloodhound , 0
 
 
 :: Build gpppfinder
-REM CALL clone.bat https://bitbucket.org/grimhacker/gpppfinder.git
-REM CALL build-py.bat cli , gpppfinder , 0
+CALL clone.bat https://bitbucket.org/grimhacker/gpppfinder.git
+CALL build-py.bat cli , gpppfinder , 0
 
 
-REM CALL clone.bat GhostPack/Rubeus
-REM CALL log.bat "Building Rubeus..."
+CALL clone.bat GhostPack/Rubeus
+CALL log.bat "Building Rubeus..."
 msbuild /property:Configuration=Release
 CALL log.bat "Create Rubeus.7z with required files..."
 cd Rubeus\bin\release
@@ -127,11 +123,11 @@ IF "%ERRORLEVEL%" == "%_errorExpected%" (
 )
 
 
-REM CALL clone.bat deepinstinct/LsassSilentProcessExit
-REM CALL log.bat "Building LsassSilentProcessExit..."
-REM msbuild /property:Configuration=Release
-REM CALL log.bat "Create LsassSilentProcessExit.7z with required files..."
-REM cd x64\Release
+CALL clone.bat deepinstinct/LsassSilentProcessExit
+CALL log.bat "Building LsassSilentProcessExit..."
+msbuild /property:Configuration=Release
+CALL log.bat "Create LsassSilentProcessExit.7z with required files..."
+cd x64\Release
 :: Running LsassSilentProcessExit will crash the script :'(
 IF EXIST LsassSilentProcessExit.exe (
 	CALL log.bat "✅ Build LsassSilentProcessExit.exe OK" 1
